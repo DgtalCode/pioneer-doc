e227b1fe230aa3c518f0fda0908b01c3fe9f20c1
mlutskiy <67636713+mlutskiy@users.noreply.github.com>
2022-11-21 18:10:05 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -20,13 +20,13 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Pioneer'
-copyright = '2021, Geoscan LTD'
+copyright = '2022, Geoscan LTD'
 author = 'Geoscan LTD'
 
 # The short X.Y version
-version = '2.4'
+version = '3.0'
 # The full version, including alpha/beta/rc tags
-release = 'September update 2021'
+release = 'Autumn update 2022'
 
 # Здесь перечисляются множественные подстановки, которые при билде будут объявлятся в конце страницы (версии,ссылки и.т.д) ----
 
